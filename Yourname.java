e6bf32d376242feb0e713fe9fdcb43bc323e87b7
694259009-creator <694259009@webmail.npru.ac.th>
2026-06-25 16:30:14 +0700

    Youname

---
+++ Yourname.java
@@ -0,0 +1,7 @@
+void main() {
+    IO.println("Enter your name");
+    String name = IO.readln();
+    IO.println("Enter your age");
+    int age = Integer.parseInt(IO.readln());
+    IO.println(" Hello "+name +" Your age is "+age +" Naja");
+}
